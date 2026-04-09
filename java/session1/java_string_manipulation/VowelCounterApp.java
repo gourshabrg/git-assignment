62a4ccdfe51b20122dccfe2abc962b632b8d551a
Gourshab <ravindragourcode786@gmail.com>
2026-04-09 21:40:57 +0530

    feat: vowel count program

---
+++ java/session1/java_string_manipulation/VowelCounterApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VowelCounterApp {
+    public static int countVowels(String input) {
+        int count = 0;
+        input = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        int vowels = countVowels(input);
+        System.out.println("Number of vowels: " + vowels);
+    }
+}
