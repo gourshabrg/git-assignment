f9003921c47f2fa168a991354ceab93260729c2a
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 23:55:54 +0530

    feat: inside exception global exception logic added

--- java/session3/springrestassignment/src/main/java/com/ravindra/springrestassignment/exception/GlobalExceptionHandler.java
+++ java/session3/springrestassignment/src/main/java/com/ravindra/springrestassignment/exception/GlobalExceptionHandler.java
@@ -1,5 +1,12 @@
 package com.ravindra.springrestassignment.exception;
 
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
-  
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex) {
+        return "Error: " + ex.getMessage();
+    }
 }
