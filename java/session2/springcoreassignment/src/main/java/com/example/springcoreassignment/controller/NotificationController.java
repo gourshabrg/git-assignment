b08fe4734b291de66ed3a9c6147b4976caa2c559
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 15:11:28 +0530

    feat add the notification controller

--- java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/controller/NotificationController.java
+++ java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/controller/NotificationController.java
@@ -1,5 +1,22 @@
 package com.example.springcoreassignment.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springcoreassignment.service.NotificationService;
+
+@RestController
 public class NotificationController {
-  
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    // Trigger notification
+    @GetMapping("/notify")
+    public String notifyUser() {
+        return notificationService.triggerNotification();
+    }
 }
