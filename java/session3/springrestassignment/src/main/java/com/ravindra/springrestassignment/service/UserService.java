7a5c16d190cf05ca32c5c59a969fed42233082df
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 23:54:11 +0530

    feat: inside service userservice logic added

--- java/session3/springrestassignment/src/main/java/com/ravindra/springrestassignment/service/UserService.java
+++ java/session3/springrestassignment/src/main/java/com/ravindra/springrestassignment/service/UserService.java
@@ -1,5 +1,33 @@
 package com.ravindra.springrestassignment.service;
 
+import com.ravindra.springrestassignment.model.User;
+import com.ravindra.springrestassignment.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class UserService {
-  
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> searchUsers(String name, Integer age, String role) {
+
+        return repository.getAllUsers().stream()
+                .filter(user ->
+                        (name == null || user.getName().equalsIgnoreCase(name)) &&
+                        (age == null || user.getAge() == age) &&
+                        (role == null || user.getRole().equalsIgnoreCase(role))
+                )
+                .collect(Collectors.toList());
+    }
+
+    public void deleteUser(int id) {
+        repository.deleteUser(id);
+    }
 }
