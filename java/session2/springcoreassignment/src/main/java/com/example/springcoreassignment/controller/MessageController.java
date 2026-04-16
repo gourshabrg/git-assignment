14aae0e90c547d0c8da645a8b9d94c274b35f157
Gourshab <ravindragourcode786@gmail.com>
2026-04-16 15:10:00 +0530

    feat: add the message controller

--- java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/controller/MessageController.java
+++ java/session2/springcoreassignment/src/main/java/com/example/springcoreassignment/controller/MessageController.java
@@ -1,5 +1,23 @@
 package com.example.springcoreassignment.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springcoreassignment.service.MessageService;
+
+@RestController
 public class MessageController {
-  
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    // get message based on type (SHORT or LONG)
+    @GetMapping("/message")
+    public String getMessage(@RequestParam String type) {
+        return messageService.getMessage(type);
+    }
 }
