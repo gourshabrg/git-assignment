5b266d4e72c3390e6b19d17ca1e657b8fcb34daf
Gourshab <ravindragourcode786@gmail.com>
2026-04-24 19:03:20 +0530

    feat: add the user entity

---
+++ Capstone_InterviewTracking/backend/InterviewTracking/src/main/java/com/Capstone/InterviewTracking/entity/User.java
@@ -0,0 +1,34 @@
+package com.Capstone.InterviewTracking.entity;
+
+import com.Capstone.InterviewTracking.enums.RoleType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+    
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public RoleType getRole() { return role; }
+    public void setRole(RoleType role) { this.role = role; }
+}
